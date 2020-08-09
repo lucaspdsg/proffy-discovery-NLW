8074d73b46c9024a68b1323f7b346c3f4cf59082
Lucas Pinheiro <lucaspifam82@gmail.com>
2020-08-08 21:54:13 -0400

    Create test.js

---
+++ src/views/utils/database/test.js
@@ -0,0 +1,63 @@
+const Database  = require('./db')
+const createProffy = require('./createProffy')
+
+Database.then(async(db) => {
+//inserir dados
+
+proffyValue = {
+    name: "Mayk Brito", 
+    avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+    whatsaap: "51900000000",
+    bio: "Instrutor de Educação Física para iniciantes, minha missão de vida é levar saúde e contribuir para o crescimento de quem se interessar. Comecei a minha jornada profissional em 2001, quando meu pai me deu dois alteres de 32kg com a seguinte condição: Aprenda a fazer dinheiro com isso!"
+}
+
+classValue = {
+subject: "1",
+cost: "40",
+//o proffy id virá pelo banco de dados
+}
+
+classScheduleValues = [
+    //class_id virá pelo banco de dados, após cadastarmos a class.
+    {
+        weekday: 1,
+        time_from: 720,
+        time_to: 1220
+    },
+    {
+        weekday: 0,
+        time_from: 520,
+        time_to: 1220
+    }
+]
+//createProffy(db, {proffyValue, classValue, classScheduleValues})
+
+//consultar os dados inseridos
+
+//todos os proffys
+const selectedProffys = await db.all("SELECT * FROM proffys")
+//console.log(selectedProffys)
+
+//consultar as classes de um determinado professor 
+// e trazer junto os dados do professor
+const selectClassesAndProffys = await db.all(`
+    SELECT classes. *, proffys.*
+    FROM proffys
+    JOIN classes ON (classes.proffy_id = proffys.id)
+    WHERE classes.proffy_id = 1;
+`)
+//console.log(selectClassesAndProffys)
+
+//o horário que a pessoa trabalha, por exemplo, é das 8h - 18h
+//o horário do time_from (8h) precisa ser menor ou igual ao horário solicitado
+//o time_to precisa ser acima 
+const selectClassesSchedules = await db.all(`
+SELECT class_schedule.*
+FROM class_schedule
+WHERE class_schedule.class_id = "1"
+AND class_schedule.weekday = "0"
+AND class_schedule.time_from <= "1300"
+AND class_schedule.time_to > "1300"
+`)
+console.log(selectClassesSchedules)
+})
