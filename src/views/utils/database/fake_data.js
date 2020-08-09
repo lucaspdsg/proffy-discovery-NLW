2d9d1be17c133b51cc9ebe8eb80425c114601ad5
Lucas Pinheiro <lucaspifam82@gmail.com>
2020-08-08 21:53:24 -0400

    Create fake_data.js

---
+++ src/views/utils/database/fake_data.js
@@ -0,0 +1,36 @@
+//Dados
+const proffys = [
+    {name: "Mayk Brito", avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+    whatsaap: "5598989898",
+    bio: "Instrutor de Educação Física para iniciantes, minha missão de vida é levar saúde e contribuir para o crescimento de quem se interessar. Comecei a minha jornada profissional em 2001, quando meu pai me deu dois alteres de 32kg com a seguinte condição: Aprenda a fazer dinheiro com isso!",
+    subject: "Educação Física",
+    cost: "40",
+    weekday: [0],
+    time_from: [720],
+    time_to: [1200]
+    },
+
+    {
+    name: "Lucas Pinheiro", 
+    avatar: "https://scontent.fpll4-1.fna.fbcdn.net/v/t1.0-9/53071314_993503157503381_8813055518802182144_o.jpg?_nc_cat=105&_nc_sid=09cbfe&_nc_eui2=AeF1S6adOxM0mtftWvd_QHjHF4j1HU61DH8XiPUdTrUMfw8-YpV3UGgdgcKhAdujSIh51KV5USGXiSGjtwXIFc5a&_nc_ohc=bf2HTkK9azUAX9RRJLb&_nc_ht=scontent.fpll4-1.fna&oh=d30abf503e9c43c021501eaeba68d4f5&oe=5F50C60D",
+    whatsaap: "5598989899",
+    bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.00 pessoas já passaram por uma das minhas explosões",
+    subject: "Química",
+    cost: "20",
+    weekday: [0],
+    time_from: [720],
+    time_to: [1200]
+    },
+
+    {
+    name: "Tiago Luchenberg", 
+    avatar: "https://mir-s3-cdn-cf.behance.net/user/115/4713a37191453.5f14e9ebb5e58.jpg",
+    whatsaap: "5598989999",
+    bio: "As vezes não sei nem onde eu tô, mas consigo me localizar facilmente em qualquer lugar. Tenho memória fotográfica e nunca fico perdido. Eu ensino a galera como não se perder na vida, com lições geográficas simples pra você nunca mais precisar de mapa na sua bela vida.",
+    subject: "Geografia",
+    cost: "360",
+    weekday: [0],
+    time_from: [720],
+    time_to: [1200]
+    }
+]
